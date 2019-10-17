271b864ea686c720d816ab1e4b0ea7d095b9da4a
Rafael Tongson <rtongson@peppymiint.local>
2019-10-16 18:34:34 -0700

    initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhoneAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
